54b556b056e1f8326a46b0f9816e5202897f144f
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-02-17 19:29:47 +0100

    Added new class Calculator and test methos

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,24 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        int addResult = calculator.add(5, 5);
+        int subtractResult = calculator.subtract(5, 5);
+
+        if (addResult == 10) {
+            System.out.println("Add method test OK");
+        } else {
+            System.out.println("Add method test Error!");
+        }
+
+        if (subtractResult == 0) {
+            System.out.println("Subtract method test OK");
+        } else {
+            System.out.println("Subtract method test Error!");
+        }
     }
 }
