5da797878a99ac61963b4c5523e4be3c84dd95ee
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-03-01 17:52:58 +0100

    Changed declaration of array named 'numbers'

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -7,7 +7,7 @@ public class ArrayOperationsTestSuite {
     @Test
     public void testGetAverage() {
         //Given
-        int numbers[] = {3, 4, 5, 2, 9, 7, 4, 7, 9, 1, 5, 8, 4, 5, 7, 3, 7, 9, 3, 6};
+        int[] numbers = {3, 4, 5, 2, 9, 7, 4, 7, 9, 1, 5, 8, 4, 5, 7, 3, 7, 9, 3, 6};
         //When
         double average = ArrayOperations.getAverage(numbers);
         //Then
