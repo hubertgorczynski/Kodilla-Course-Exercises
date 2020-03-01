4e6243081b1ff2cd5c0a385acd354c931013a9f9
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-03-01 15:16:54 +0100

    Exercise 7.5

---
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArrayOperationsTestSuite {
+    @Test
+    public void testGetAverage() {
+        //Given
+        int numbers[] = {3, 4, 5, 2, 9, 7, 4, 7, 9, 1, 5, 8, 4, 5, 7, 3, 7, 9, 3, 6};
+        //When
+        double average = ArrayOperations.getAverage(numbers);
+        //Then
+        Assert.assertEquals(5.4, average, 0.0001);
+    }
+}
