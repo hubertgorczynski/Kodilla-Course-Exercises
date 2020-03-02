7308f487f5a3d29f55d4f3fe57696e799a52b5d4
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-03-02 21:04:22 +0100

    Exercise 8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -1,7 +1,6 @@
 package com.kodilla.exception.test;
 
 public class ExceptionHandling {
-
     public static void main(String[] args) {
         SecondChallenge secondChallenge = new SecondChallenge();
 
