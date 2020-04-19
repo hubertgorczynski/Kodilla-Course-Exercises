73bd42a48b2b3937ac00cd454174c97e00ffa87c
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-04-19 19:06:53 +0200

    Removed spring adnotations in

--- src/test/java/com/kodilla/KodillaCourseApplicationTests.java
+++ src/test/java/com/kodilla/KodillaCourseApplicationTests.java
@@ -1,13 +1,7 @@
 package com.kodilla;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class KodillaCourseApplicationTests {
+    public static void main(String[] args) {
 
-    @Test
-    void contextLoads() {
     }
-
 }
