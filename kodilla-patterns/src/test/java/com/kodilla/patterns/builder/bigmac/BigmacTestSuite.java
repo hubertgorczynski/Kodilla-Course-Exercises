8bcc8689d90b28e4322f28cac77ae556eff71078
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-04-20 15:10:12 +0200

    Exercise 15.5 Builder

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("Sesame seed bun")
+                .burgers(2)
+                .sauce("Barbecue")
+                .ingredient("Bacon")
+                .ingredient("Pickles")
+                .ingredient("Onion")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        int howManyBurgers = bigmac.getBurgers();
+        String whichSauce = bigmac.getSauce();
+        //Then
+        Assert.assertEquals(3, howManyIngredients);
+        Assert.assertEquals(2, howManyBurgers);
+        Assert.assertEquals("Barbecue", whichSauce);
+        Assert.assertTrue(bigmac.getIngredients().contains("Pickles"));
+        Assert.assertFalse(bigmac.getIngredients().contains("Mushrooms"));
+    }
+}
