3134052b380e9311e9d0b8b14450039b57ebc6f3
hubertgorczynski <hubert.gorczynski378@gmail.com>
2020-08-14 17:40:22 +0200

    Created field statistics in MedianAdaptee class

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdaptee.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdaptee.java
@@ -8,15 +8,15 @@ import com.kodilla.patterns2.adapter.bookclasifier.libraryb.Statistics;
 import java.util.Map;
 
 public class MedianAdaptee implements BookStatistics {
+    private Statistics statistics;
+
     @Override
     public int averagePublicationYear(Map<BookSignature, Book> books) {
-        Statistics statistics = new Statistics();
         return statistics.averagePublicationYear(books);
     }
 
     @Override
     public int medianPublicationYear(Map<BookSignature, Book> books) {
-        Statistics statistics = new Statistics();
         return statistics.medianPublicationYear(books);
     }
 }
